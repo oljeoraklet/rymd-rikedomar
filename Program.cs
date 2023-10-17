676b256fc78d9e0bbbf785b1031264665fbc3218
_oraklet <olle_wikstrom@hotmail.com>
2023-10-17 15:37:31 +0200

    Abstracted TradingStation to own class

--- Program.cs
+++ Program.cs
@@ -9,35 +9,6 @@ using RymdRikedomar.Entities.SpaceShip.Modules;
 namespace SpaceConsoleMenu
 {
 
-    public class TradingStation
-    {
-        public List<(IGood Good, int Stock)> AvailableGoods { get; set; }
-
-        public TradingStation()
-        {
-            AvailableGoods = new List<(IGood, int)>
-        {
-            (new Spice(), 100),
-            (new Metal(), 100)
-        };
-        }
-
-        public (IGood Good, int Stock)? FindGoodByName(string name)
-        {
-            return AvailableGoods.FirstOrDefault(g => g.Good.Name == name);
-        }
-
-        public void UpdateStock(IGood good, int newStock)
-        {
-            var item = AvailableGoods.FirstOrDefault(g => g.Good == good);
-            if (item.Good != null)
-            {
-                AvailableGoods.Remove(item);
-                AvailableGoods.Add((item.Good, newStock));
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
